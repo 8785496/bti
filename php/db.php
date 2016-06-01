c4046bdc0cbc3c5561949c570c4daeb1021164e2
8785496 <georg.88@mail.ru>
2016-06-01 16:42:47 +0600

    edit db.php

--- php/db.php
+++ php/db.php
@@ -1,12 +1,6 @@
 <?php
 
-//$host = 'mysql.hostinger.ru';
-//$dbname = 'u164997131_bti';
-//$username = 'u164997131_bti';
-//$password = 'SnINAf4uRu';
-
 $host = 'localhost';
 $dbname = 'bti';
 $username = 'root';
 $password = '';
-
