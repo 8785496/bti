790adc3d00251f9f0fbd2c6dcf0477016d596f77
8785496 <georg.88@mail.ru>
2016-07-07 16:37:13 +0600

    edit metrika

--- temlate/metrika.php
+++ temlate/metrika.php
@@ -1,10 +1,10 @@
-<!--<script type="text/javascript">
+<script type="text/javascript">
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function() {
             try {
                 w.yaCounter36447135 = new Ya.Metrika({
                     id:36447135,
-                    clickmap:true,
+                    clickmap:false,
                     trackLinks:true,
                     accurateTrackBounce:true
                 });
@@ -27,5 +27,22 @@
     <div>
         <img src="https://mc.yandex.ru/watch/36447135" style="position:absolute; left:-9999px;" alt="" />
     </div>
-</noscript>-->
+</noscript>
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-77840643-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
         
